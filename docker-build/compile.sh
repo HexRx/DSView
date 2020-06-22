be0e384fc49045b9c131b2fe123d989df027e47f
HexRx <bps.programmer@gmail.com>
2020-06-22 15:03:28 +0300

    Add linuxdeployqt tool to create AppImage

--- docker-build/compile.sh
+++ docker-build/compile.sh
@@ -17,3 +17,16 @@ cd ~/DSView
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr .
 make -j$(nproc)
 make install DESTDIR=~/AppDir
+
+# create appimage
+sudo ldconfig
+
+cd ~
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+
+./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/*.desktop -appimage
+
+rm -r /host/build
+mkdir /host/build/
+mv DSView*.AppImage /host/build/
